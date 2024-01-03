797be6dc6985a42ff4a62a572214e3bbdf279311
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2024-01-03 15:54:09 -0600

    feat(componentes): add images to SelectType

--- src/components/SelectType.tsx
+++ src/components/SelectType.tsx
@@ -1,10 +1,24 @@
 export const SelectType = () => {
-    const productsType = ["Muslo", "Entero", "Pechuga"]
+    const productsType = [
+        {
+            name:"Muslo",
+            img:"muslo-pierna.png"
+        },
+        {
+            name:"Entero",
+            img:"entero.png"
+        },
+        {
+            name:"Pechuga",
+            img: "pechuga.png"
+        }
+    ]
     return (
         <div className="flex gap-2 py-4">
             {productsType.map( pro => (
-                <div className="flex-1 text-xl p-2 bg-gray-300 rounded">
-                    {pro}
+                <div className="flex-1 flex justify-center items-center h-[85px] text-xl p-2 bg-gray-100 rounded hover:bg-gray-300">
+                    <img className="h-full" src={pro.img} alt={pro.name}/>
+                    {/* <p>{pro.name}</p> */}
                 </div>
             ))}
         </div>
